ad60c08ae8dda0e867a4a85778dc987fb58c2356
Sara Bracha <stanenba2@student.touro.edu>
2016-09-19 20:02:27 -0400

    Now it adds

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		//instantiate keyboard as Scanner
+		Scanner keyboard = new Scanner(System.in);
+		
+		//obtain user input
+		System.out.println("Welcome to the Adder!");
+		System.out.println("Please enter the first number you would like to add");
+		double num1 = keyboard.nextDouble();
+		System.out.println("Please enter the second number");
+		double num2 = keyboard.nextDouble();
+		
+		//add!
+		double sum = num1 + num2;
+		System.out.println("The sum is " + sum);
+		
 	}
 }
