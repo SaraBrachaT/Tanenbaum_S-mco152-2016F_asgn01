4917f60ea52be9d8e570cba3a3bfb4e7c97b000f
Sara Bracha <stanenba2@student.touro.edu>
2016-09-19 22:38:27 -0400

    Now made Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,13 +1,19 @@
 
-public class Subtractor {
-
+public class Calculator {
+	
 	private double num1;
 	private double num2;
+	private double sum;
 	private double difference;
 	
-	public Subtractor(double numb1, double numb2){
+	public Calculator(double numb1, double numb2){
 		this.num1 = numb1;
 		this.num2 = numb2;
+	}
+	
+	public double add(){
+		this.sum = this.num1 + this.num2;
+		return sum;
 	}
 	
 	public double subtract(){
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,16 +8,29 @@ public class HelloWorld {
 		
 		//obtain user input
 		System.out.println("Welcome to the Add and Subtract Calculator!");
+		
+		int choice;
+		do{
 		System.out.println("Please enter the first number");
 		double num1 = keyboard.nextDouble();
 		System.out.println("Please enter the second number");
 		double num2 = keyboard.nextDouble();
+		System.out.println("Press 1 to add these numbers and 2 to subtract them. Press 3 to quit");
+		choice = keyboard.nextInt();
 		
-		//add!
-		Adder add = new Adder(num1, num2);
-		Subtractor sub = new Subtractor(num1, num2);
-		System.out.println("The sum of " + num1 + " and " + num2 + " is " + add.add());
-		System.out.println("And " + num1 + " minus " + num2 + " is " + sub.subtract());
+		//perform calculation
+		Calculator calc = new Calculator(num1, num2);
+		switch(choice){
+			case 1:
+				System.out.println("The sum of " + num1 + " and " + num2 + " is " + calc.add());
+				break;
+			case 2:
+				System.out.println("And " + num1 + " minus " + num2 + " is " + calc.subtract());
+				break;
+		}
+		} //end do
+		while(choice != 3);
 		
+		System.out.println("Thank you for using the Add and Subtract Calculator");
 	}
 }
